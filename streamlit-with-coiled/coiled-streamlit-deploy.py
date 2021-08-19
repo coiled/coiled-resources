56e157d25b698e7738ee759e55b27302d4608c4b
Richard Pelgrim <68642378+rrpelgrim@users.noreply.github.com>
2021-08-19 13:55:45 +0200

    Update coiled-streamlit-deploy.py

--- streamlit-with-coiled/coiled-streamlit-deploy.py
+++ streamlit-with-coiled/coiled-streamlit-deploy.py
@@ -186,6 +186,4 @@ st.write(
 )
 
 if st.button('Shutdown Cluster'):
-    with st.spinner("This functionality is disabled for this public example to ensure a smooth experience for all users."):
-        
-
+    st.write("This functionality is disabled for this public example to ensure a smooth experience for all users.")    
